2279f339a5351dd73682182f120d4e9b8afe7307
Coastony <sergiobsantos8@gmail.com>
2023-01-14 00:29:59 -0300

    Primeiro Commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <h1>My.<span>finances</span></h1>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,25 @@
+export const defaultTheme = {
+  white: '#fff',
+  black: '#000',
+  gray: '#222831',
+  gray_hover: '#15191f',
+  gray_hover2: '#090a0d',
+  back: '#222831',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#ebebf5',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#393E46',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#EEEEEE',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#00ADB5',
+
+  'red-300': '#F75A68',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+} as const
